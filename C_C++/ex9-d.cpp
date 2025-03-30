905f912c00efd0700a742a7d6ebc54b6d020e148
chiyanglin <chiyanglin@gmail.com>
2025-03-30 13:48:51 +0800

    Create ex9-d.cpp

---
+++ C_C++/ex9-d.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int x = 2;
+
+    switch (x) {
+        case 1:
+            std::cout << "One" << std::endl;
+            break;
+        case 2:
+            std::cout << "Two" << std::endl;
+            break;
+        default:
+            std::cout << "Other" << std::endl;
+    }
+
+    return 0;
+}
